812f22f44bbb29942d14ba68566a4b57b1fa30fe
Lewis Thorley <lewis.thorley@netlify.com>
2024-02-20 13:47:33 +0000

    chore: try without schema

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 // Documentation: https://sdk.netlify.com
 import { NetlifyIntegration, z } from "@netlify/sdk";
 
-const buildConfigSchema = z.object({
-  some_setting: z.string().optional(),
-});
-
-const integration = new NetlifyIntegration({ buildConfigSchema });
+// const buildConfigSchema = z.object({
+//   some_setting: z.string().optional(),
+// });
+//
+const integration = new NetlifyIntegration();
 
 integration.onEnable(async (_, { teamId, siteId, client }) => {
   // Build event handlers are disabled by default, so we need to
